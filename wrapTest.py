39382ecb92e96800f9653ac9ded479a80b05e198
Ebba Hermelin <ebbahermelin@Ebbas-MacBook-Air.local>
2017-12-25 00:02:23 +0100

    sorterat tester, se drive test kalkylark

---
+++ wrapTest.py
@@ -0,0 +1,21 @@
+import stringProvider
+import code
+import unittest
+import re
+import textwrap
+from textWrap_example import sample_text
+
+
+
+def wrap(text, width=70, **kwargs):
+#Wrap a single paragraph of text, returning a list of wrapped lines.
+    return wrap
+
+class testStr(unittest.TestCase):
+#testa om en line är längre än 70 width
+    def testWrap(self):
+        self.assertEqual(wrap(sample_text), seventyone)
+
+
+if __name__ == '__main__':
+    unittest.main()
