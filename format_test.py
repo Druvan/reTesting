e3d9a7e1e47597a41290140a25c24ce63df00ec0
Druvan <edvin.lundberg@gmail.com>
2017-12-26 13:01:59 +0100

    Har kommit på alla tester som jag kan

--- format_test.py
+++ format_test.py
@@ -26,6 +26,7 @@ class TestString(unittest.TestCase):
         self.assertEqual('{0}'.format('a','b','c'), "a")
         self.assertEqual(indexes.format('a','b','c'), "a,b,c")
         self.assertEqual('{0},{1},{2}'.format(*longString), "a,b,a")
+        self.assertEqual('{},{},{}'.format(*longString), "a,b,a")
         self.assertEqual('{2},{1},{0}'.format(*longString), "a,b,a")
         indexes = createIndexes(range(len(longString)))
         self.assertEqual(indexes.format(*longString), sp.stringMultiply("a,b,",10000))
@@ -51,11 +52,56 @@ class TestString(unittest.TestCase):
         self.assertEqual('{:>1}'.format("right"), 'right') 
         self.assertEqual('{:^10}'.format("center"), '  center  ') 
         self.assertEqual('{:^5}'.format("center"), 'center') 
+        self.assertEqual('{:=5}'.format(-3), '-   3') 
+        self.assertEqual('{:=5}'.format(+3), '    3') 
+        self.assertEqual('{:0=5}'.format(+3), '00003') 
+        self.assertEqual('{:=5}'.format(3), '    3') 
+
+
+
+        self.assertEqual('{:e}'.format(311111111), '3.111111e+08') 
+        self.assertEqual('{:.1e}'.format(311111111), '3.1e+08') 
+
+        self.assertEqual('{:E}'.format(311111111), '3.111111E+08') 
+        self.assertEqual('{:.1E}'.format(311111111), '3.1E+08')
 
         self.assertEqual('{:+f} {:+f}'.format(13.37, -13.37), '+13.370000 -13.370000') 
         self.assertEqual('{: f} {: f}'.format(13.37, -13.37), ' 13.370000 -13.370000') 
         self.assertEqual('{:-f} {:-f}'.format(13.37, -13.37), '13.370000 -13.370000') 
         self.assertEqual('{:f} {:f}'.format(13.37, -13.37), '13.370000 -13.370000') 
+
+        self.assertEqual('{:+.2f} {:+.2f}'.format(13.37, -13.37), '+13.37 -13.37') 
+        self.assertEqual('{: .2f} {: .2f}'.format(13.37, -13.37), ' 13.37 -13.37') 
+        self.assertEqual('{:-.2f} {:-.2f}'.format(13.37, -13.37), '13.37 -13.37') 
+        self.assertEqual('{:.2f} {:.2f}'.format(13.37, -13.37), '13.37 -13.37') 
+
+        self.assertEqual('{:+.2F} {:+.2F}'.format(13.37, -13.37), '+13.37 -13.37') 
+        self.assertEqual('{: .2F} {: .2F}'.format(13.37, -13.37), ' 13.37 -13.37') 
+        self.assertEqual('{:-.2F} {:-.2F}'.format(13.37, -13.37), '13.37 -13.37') 
+        self.assertEqual('{:.2F} {:.2F}'.format(13.37, -13.37), '13.37 -13.37') 
+        # self.assertEqual('{:.2F} {:.2F}'.format('inf', '-nan'), 'INF -NAN') Fattar inte hur den funkar
+
+        self.assertEqual('{:.0g} {:.1g}'.format(13.37, -13.37), '1e+01 -1e+01') 
+        self.assertEqual('{:.4g} {:.5g}'.format(13.37, -13.37), '13.37 -13.37') 
+        self.assertEqual('{:g} {:g}'.format(13.37, -13.37), '13.37 -13.37') 
+        # self.assertEqual('{:.10g} {:.17g}'.format(13.37, -13.37), '13.37 -13.37') Funkar inte för 17 och högre
+        # self.assertEqual('{:.100g} {:.1000g}'.format(13.37, -13.37), '13.37 -13.37') BUGGG!!!!
+
+        # Funkar inte för 17 eller högre
+        self.assertEqual('{:.10n} {:.16n}'.format(13.37, -13.37), '13.37 -13.37') 
+        self.assertEqual('{:.0n} {:.1n}'.format(13.37, -13.37), '1e+01 -1e+01') 
+        self.assertEqual('{:.4n} {:.5n}'.format(13.37, -13.37), '13.37 -13.37') 
+        self.assertEqual('{:n} {:n}'.format(13.37, -13.37), '13.37 -13.37') 
+
+        self.assertEqual('{:%}'.format(13.37/-13.37), '-100.000000%') 
+        self.assertEqual('{:.0%}'.format(13.37/-13.37), '-100%') 
+        self.assertEqual('{:.20%}'.format(3/2), '150.00000000000000000000%') 
+        self.assertEqual('{:.80%}'.format(3/2), '150.00000000000000000000000000000000000000000000000000000000000000000000000000000000%') 
+
+        # Funkar inte för 17 eller högre
+        self.assertEqual('{:.0} {:.3}'.format(13.37, -13.37), '1e+01 -13.4') 
+        self.assertEqual('{:.10} {:.16}'.format(13.37, -13.37), '13.37 -13.37') 
+
         self.assertEqual('{0:d},{0:x},{0:o},{0:b}'.format(15), '15,f,17,1111') 
         self.assertEqual('{0:d},{0:x},{0:o},{0:b}'.format(0), '0,0,0,0') 
         self.assertEqual('{0:d},{0:x},{0:o},{0:b}'.format(-2), '-2,-2,-2,-10') 
@@ -66,6 +112,8 @@ class TestString(unittest.TestCase):
         self.assertEqual('{:,}'.format(100000), '100,000')
         self.assertEqual('{:,}'.format(-100000), '-100,000')
         self.assertEqual('{:,}'.format(0), '0')
+
+        self.assertEqual('{0[2]},{0[1]},{0[0]},{1[0]},{1[1]},{1[2]}'.format((1,2,3),(4,5,6)), '3,2,1,4,5,6')
 
 
 if __name__ == '__main__':
