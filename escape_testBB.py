cbfd1e6545dc97f1f69e30aeb82b99c7367ea77f
Druvan <edvin.lundberg@gmail.com>
2018-01-06 13:21:54 +0100

    Added byte array tests

--- escape_testBB.py
+++ escape_testBB.py
@@ -7,6 +7,7 @@ class TestString(unittest.TestCase):
     def testEscapeBB(self):
 
         longString = stringProvider.testSubHelperA()
+        self.assertEqual(re.escape(""), "")
         self.assertEqual(re.escape("a"), "a")
         self.assertEqual(re.escape("!"), "\!")
         self.assertEqual(re.escape("hsdjkahsdakjhsdh"), "hsdjkahsdakjhsdh")
@@ -23,6 +24,26 @@ class TestString(unittest.TestCase):
         self.assertEqual(re.escape(longString), longString)
         self.assertEqual(re.escape("%"+longString), "\%"+longString)
         self.assertEqual(re.escape(longString+"%"), longString+"\%")
+
+        with self.assertRaises(TypeError):
+            self.assertEqual(re.escape(1), "1")
+            self.assertEqual(re.escape(), "")
+
+        self.assertEqual(re.escape(b""), b"")
+        self.assertEqual(re.escape(bytearray('a', 'ascii')), b"a")
+        self.assertEqual(re.escape(bytearray('!', 'ascii')), b"\!")
+        self.assertEqual(re.escape(bytearray('hsdjkahsdakjhsdh', 'ascii')), b"hsdjkahsdakjhsdh")
+        self.assertEqual(re.escape(bytearray('hsdjkahsdak jhsdh', 'ascii')), b"hsdjkahsdak\\ jhsdh")
+        self.assertEqual(re.escape(bytearray('/hsdjkahsdakjhsdh', 'ascii')), b"\/hsdjkahsdakjhsdh")
+        self.assertEqual(re.escape(bytearray('?#!', 'ascii')), b"\?\#\!")
+        self.assertEqual(re.escape(bytearray('hej1020(', 'ascii')), b"hej1020\(")
+        self.assertEqual(re.escape(bytearray('_(', 'ascii')), b"_\(")
+        self.assertEqual(re.escape(bytearray('!hej1020(', 'ascii')), b"\!hej1020\(")
+        self.assertEqual(re.escape(bytearray('\(', 'ascii')), b"\\\\\(")
+        self.assertEqual(re.escape(bytearray(longString, 'ascii')), bytearray(longString, 'ascii'))
+        self.assertEqual(re.escape(bytearray("%"+longString, 'ascii')), bytearray("\%"+longString, 'ascii'))
+        self.assertEqual(re.escape(bytearray(longString+"%", 'ascii')), bytearray(longString+"\%", 'ascii'))
+
 
 
 if __name__ == '__main__':
