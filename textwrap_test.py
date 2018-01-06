f274ae2a89067c5485878770a6892737b71999cf
Ebba Hermelin <ebbahermelin@nl119-148-25.student.uu.se>
2018-01-06 17:56:31 +0100

    meeeeeeeera textwrap tester

--- textwrap_test.py
+++ textwrap_test.py
@@ -13,31 +13,38 @@ cccccccccccccccccccc
 value_extralong = """aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"""
 
+shorten = "aaaaa bbbbbb"
+shorten_long = "aaaaaaaaaa aaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
-wrap_extralong = textwrap.wrap(value, width=1)
-print(len(value_extralong))
-print(len(wrap_extralong))
-
-# Wrap this text.
-list = textwrap.wrap(value, width=10)
-
-# Print each line.
-for element in list:
-    print(element)
 
 class TestString(unittest.TestCase):
 
-    
+    #WRAP
     def testWrap(self): 
         self.assertEqual(textwrap.wrap(value, width=10), ['aaaaaaaaaa', 'bbbbbbbbbb', 'cccccccccc', 'cccccccccc'])
         self.assertEqual(textwrap.wrap(value, width=5), ['aaaaa', 'aaaaa', 'bbbbb', 'bbbbb', 'ccccc', 'ccccc', 'ccccc', 'ccccc'])
         self.assertEqual(textwrap.wrap(value_long, width=20), ['aaaaaaaaaabbbbbbbbbb', 'cccccccccccccccccccc'])
         self.assertEqual(len(textwrap.wrap(value, width=10)), 4)
         self.assertEqual(len(textwrap.wrap(value, width=2)), 20)
-    
-        
-      
 
+    #FILL    
+    def testFill(self):
+        self.assertEqual(textwrap.fill(value, width=10), ('aaaaaaaaaa\nbbbbbbbbbb\ncccccccccc\ncccccccccc'))
+        self.assertEqual(textwrap.fill(value, width=5), ('aaaaa\naaaaa\nbbbbb\nbbbbb\nccccc\nccccc\nccccc\nccccc'))
+        self.assertEqual(textwrap.fill(value_long, width=20), ('aaaaaaaaaabbbbbbbbbb\ncccccccccccccccccccc'))
+        self.assertEqual(len(textwrap.fill(value, width=10)), 43)
+        self.assertEqual(len(textwrap.fill(value, width=2)), 59)
+
+    #Shorten
+    def testShorten(self):
+        self.assertEqual(textwrap.shorten(shorten, width=11), 'aaaaa [...]')
+        self.assertEqual(textwrap.shorten(shorten, width=10, placeholder="..."), 'aaaaa...')
+        self.assertEqual(textwrap.shorten(shorten_long, width=20), 'aaaaaaaaaa [...]')
+        self.assertEqual(len(textwrap.shorten(shorten, width=11)), 11)
+        self.assertEqual(len(textwrap.shorten(shorten, width=10, placeholder="...")), 8)
+        
+        
+        
 
 if __name__ == '__main__':
    unittest.main()
