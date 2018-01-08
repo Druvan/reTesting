585886e6670ad20269c56593b06fee445b34a4b9
matildatrodin <matilda@trodin.com>
2018-01-08 16:54:42 +0100

    La till ett nytt case med en karaktär

--- test_subn.py
+++ test_subn.py
@@ -6,9 +6,10 @@ value = "aaaaa"
 value_caps = "aaaaa AAAAA"
 
 
-class SubnTest(unittest.TestCase):
+class subnTest(unittest.TestCase):
     def test_subn(self):
 
+        self.assertEqual(re.subn("a", "b", "a"), ("b", 1))
         self.assertEqual(re.subn("a", "b", value), ("bbbbb", 5))
         self.assertEqual(re.subn("a", "b", value_caps), ("bbbbb AAAAA", 5))
         self.assertEqual(re.subn("a", "b", value, 3, 1), ("bbbaa", 3))
