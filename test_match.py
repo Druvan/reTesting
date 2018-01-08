9feed930617887f739e8933c6ee95649e5e20df7
matildatrodin <matilda@trodin.com>
2018-01-08 16:27:54 +0100

    Kan behöva fler testcases

---
+++ test_match.py
@@ -0,0 +1,23 @@
+import unittest
+import re
+
+value = "aabaa"
+
+
+class MatchTest(unittest.TestCase):
+    def test_match(self):
+
+        self.assertEqual(re.match("b", value), None)
+        self.assertEqual(re.match("a", value).span(), (0, 1))
+        self.assertEqual(re.match("c", value), None)
+        self.assertEqual(re.match("1", value), None)
+        self.assertEqual(re.match("", "").span(), (0, 0))
+        self.assertEqual(re.match("a", ""), None)
+        self.assertEqual(re.match("@", "@%").span(), (0, 1))
+        self.assertRaises(TypeError, re.match, "a", 1)
+        self.assertRaises(TypeError, re.match, "a", True)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
